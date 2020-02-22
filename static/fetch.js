91ce80fe21562b56e16c256b50d67dac30a9d4ff
Agurato <mail.vmonot@gmail.com>
2020-02-22 21:21:49 +0100

    better client display

--- static/fetch.js
+++ static/fetch.js
@@ -31,17 +31,28 @@ function startFetch() {
             }
         
             let td_name = document.querySelector("#" + tr_id + " .name");
-            td_name.innerHTML = container_data.name;
+            td_name.innerHTML = container_data.name.substring(1);
             let td_cpu = document.querySelector("#" + tr_id + " .cpu");
-            td_cpu.innerHTML = container_data.cpu;
+            td_cpu.innerHTML = container_data.cpu.toFixed(2) + "%";
             let td_mem = document.querySelector("#" + tr_id + " .mem");
-            td_mem.innerHTML = container_data.memory + " / " + container_data.memoryLimit;
+            td_mem.innerHTML = getBytes(container_data.memory) + " / " + getBytes(container_data.memoryLimit);
             let td_memperc = document.querySelector("#" + tr_id + " .memperc");
-            td_memperc.innerHTML = container_data.memoryPercent;
+            td_memperc.innerHTML = container_data.memoryPercent.toFixed(2) + "%";
             let td_net = document.querySelector("#" + tr_id + " .net");
-            td_net.innerHTML = container_data.netIn + " / " + container_data.netOut;
+            td_net.innerHTML = getBytes(container_data.netIn) + " / " + getBytes(container_data.netOut);
             let td_block = document.querySelector("#" + tr_id + " .block");
-            td_block.innerHTML = container_data.blockIn + " / " + container_data.blockOut;
+            td_block.innerHTML = getBytes(container_data.blockIn) + " / " + getBytes(container_data.blockOut);
         }
     };
+}
+
+function getBytes(input) {
+    let mebibytes = (input / 1024) / 1024;
+    let gibibytes = mebibytes / 1024;
+    if (mebibytes < 1000) {
+        return mebibytes.toString().substring(0, 5)+ " MiB"
+    }
+    else {
+        return gibibytes.toString().substring(0, 5)+" GiB"
+    }
 }
